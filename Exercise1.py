ce9a6bb68d0308ed835341e20151aa6d6bc590d4
leo@wsl2 <l.ricker93@web.de>
2024-11-16 17:52:17 +0100

    added objective function

--- Exercise1.py
+++ Exercise1.py
@@ -11,11 +11,16 @@ FILENAME: str = os.path.join(DIRNAME, 'test_instances/small/inst_50_4_00001')
 if __name__ == '__main__':
     parser = get_settings_parser()
     parser.set_defaults(mh_titer=1000)
-    ### init problem instance
-    mWCCPInstance = MWCCPInstance.MWCCPInstance("instance") #FILENAME
+    ###INIT
+    mWCCPInstance = MWCCPInstance.MWCCPInstance("instance")  # FILENAME
     mWCCPInstance.set_problem_instance()
     mWCCPSolution = MWCCPSolution.MWCCPSolution(mWCCPInstance, len(mWCCPInstance.get_instance()["u"]))
     mWCCPInstance.get_instance()
     mWCCPInstance.draw_instance()
 
+    print(mWCCPSolution.calc_objective())
+
+
     # #run_optimization('MWCCP', MWCCPInstance, MWCCPSolution, "instance")
+
+
--- MWCCPSolution.py
+++ MWCCPSolution.py
@@ -1,11 +1,28 @@
 from pymhlib.permutation_solution import PermutationSolution
+
 from MWCCPInstance import MWCCPInstance
 
 
 class MWCCPSolution(PermutationSolution):
+    instance_w: list[list[int]]
+    instance_u: list[int]
+    instance_v: list[int]
+    instance_c: list[(int, int)]
+    instance_edges: list[(int, int, int)]
 
     def __init__(self, inst: MWCCPInstance, length: int, **kwargs):
         super().__init__(len(inst.get_instance()["u"]), inst=inst.get_instance())
+        self.instance_w = inst.get_instance()["w"]
+        self.instance_u = inst.get_instance()["u"]
+        self.instance_v = inst.get_instance()["v"]
+        self.instance_c = inst.get_instance()["c"]
+
+        edges = []
+        for u, i in enumerate(self.instance_w[1:]):
+            for v_index, weight in enumerate(i):
+                if weight > 0:
+                    edges.append((u + 1, v_index, weight))
+        self.instance_edges = edges
 
     def copy(self):
         sol = MWCCPSolution(self.inst)
@@ -13,8 +30,12 @@ class MWCCPSolution(PermutationSolution):
         return sol
 
     def calc_objective(self):
-        distance = 0
-        for i in range(self.inst.n - 1):
-            distance += self.inst.distances[self.x[i]][self.x[i + 1]]
-        distance += self.inst.distances[self.x[-1]][self.x[0]]
-        return distance
+        total_crossings = 0
+        objective_value= 0
+        position = {v: i for i, v in enumerate(self.instance_v)}
+        for (u1, v1, weight) in self.instance_edges:
+            for (u2, v2, weights) in self.instance_edges:
+                if u1 < u2 and position[v1] > position[v2]:
+                    total_crossings += 1
+                    objective_value += weight + weights
+        return (total_crossings, objective_value)
