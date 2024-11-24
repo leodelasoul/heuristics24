71daf3ae05ede6fa24fdfca27a2e87dbc1755195
Leonie Theresa Greber <e11801674@student.tuwien.ac.at>
2024-11-24 17:00:00 +0100

    working solution for local search with swap move and 3 step improvements

---
+++ v2_Exercise1.py
@@ -0,0 +1,94 @@
+import logging
+import os
+
+from pymhlib.gvns import GVNS
+from pymhlib.log import init_logger
+from pymhlib.scheduler import Method
+from pymhlib.settings import parse_settings, settings, get_settings_parser, get_settings_as_str
+
+from v2_MWCCPInstance import *
+from v2_MWCCPSolution import *
+
+DIRNAME = os.path.dirname(__file__)
+FILENAME: str = os.path.join(DIRNAME, 'test_instances/small/inst_50_4_00001')
+FILENAME_COMPET: str = os.path.join(DIRNAME, 'competition_instances/inst_50_4_00001')
+FILENAME_COMPET_2: str = os.path.join(DIRNAME, 'competition_instances/inst_200_20_00001')
+# FILENAME_LARGE: str = os.path.join(DIRNAME, 'test_instances/la')
+if __name__ == '__main__':
+    parser = get_settings_parser()
+    parser.set_defaults(mh_titer=10) # number of iterations
+    parser.set_defaults(mh_ttime=1) # time limit
+
+    ###INIT
+    mWCCPInstance = v2_MWCCPInstance(FILENAME_COMPET_2)  # FILENAME
+    mWCCPSolution = v2_MWCCPSolution(mWCCPInstance)
+
+    ###Parser arguments
+    parser.add_argument("--alg", type=str, default='gvns', help='optimization algorithm to be used '
+                                                                '(gvns, alns, pbig, par_alns, ssga, sa)')
+    parser.add_argument("--inst_file", type=str, default=mWCCPInstance,
+                        help='problem instance file')
+    parser.add_argument("--meths_ch", type=int, default=1,
+                        help='number of construction heuristics to be used')
+    parser.add_argument("--meths_li", type=int, default=1,
+                        help='number of local improvement methods to be used')
+    parser.add_argument("--meths_sh", type=int, default=5,
+                        help='number of shaking methods to be used')
+    
+    #change manually for step function
+    parser.add_argument("--meths_ls_step", type=str, default='first',
+                        help='which step function should be used for local search'
+                        '(first, best, random)')
+    parse_settings(None, 0)
+
+    init_logger()
+    logger = logging.getLogger("pymhlib")
+    logger.info("pymhlib demo for solving %s", "MWCCP")
+    logger.info(get_settings_as_str())
+    logger.info("%s instance read:\n%s", "MWCCP", str(mWCCPInstance))
+
+    #util.text = "Initial probleminstance"
+    #util.draw_instance(u=mWCCPSolution.instance_u, x=mWCCPSolution.x, w=mWCCPSolution.instance_w)
+    
+    #
+    # alg = GVNS(mWCCPSolution,
+    #            [Method(f"construct{i}", MWCCPSolution.construct, i) for i in range(settings.meths_ch)],
+    #            [Method(f"local-2opt{i}", MWCCPSolution.local_improve, i) for i in range(1, settings.meths_li + 1)],
+    #            [Method(f"sh{i}", MWCCPSolution.shaking, i) for i in range(1, settings.meths_sh + 1)],
+    #            None, False)
+    #
+
+    if settings.meths_ls_step == 'first':
+        alg = GVNS(mWCCPSolution,
+               [Method(f"construct{i}", v2_MWCCPSolution.construct, i) for i in range(settings.meths_ch)],
+               [Method(f"local-2opt{i}", v2_MWCCPSolution.ls_two_swap_first, i) for i in range(1, settings.meths_li + 1)],
+               [],
+               None, False)
+    elif settings.meths_ls_step == 'best':
+        alg = GVNS(mWCCPSolution,
+               [Method(f"construct{i}", v2_MWCCPSolution.construct, i) for i in range(settings.meths_ch)],
+               [Method(f"local-2opt{i}", v2_MWCCPSolution.ls_two_swap_best, i) for i in range(1, settings.meths_li + 1)],
+               [],
+               None, False)
+    elif settings.meths_ls_step == 'random':
+        alg = GVNS(mWCCPSolution,
+               [Method(f"construct{i}", v2_MWCCPSolution.construct, i) for i in range(settings.meths_ch)],
+               [Method(f"local-2opt{i}", v2_MWCCPSolution.ls_two_swap_random, i) for i in range(1, settings.meths_li + 1)],
+               [],
+               None, False)
+        
+    else: 
+        alg = GVNS(mWCCPSolution,
+               [Method(f"construct{i}", v2_MWCCPSolution.construct, i) for i in range(settings.meths_ch)],
+               [Method(f"local-2opt{i}", v2_MWCCPSolution.ls_two_swap_first, i) for i in range(1, settings.meths_li + 1)],
+               [],
+               None, False)
+
+    alg.run()
+    logger.info("")
+    alg.method_statistics()
+    alg.main_results()
+
+    # util.text = "Applied heuristic optimization methods"
+    # util.draw_instance(u=mWCCPSolution.instance_u, x=mWCCPSolution.x, w=mWCCPSolution.instance_w)
+
---
+++ v2_MWCCPSolution.py
@@ -0,0 +1,349 @@
+import random
+from typing import Any
+
+import numpy as np
+from pymhlib.permutation_solution import PermutationSolution
+from pymhlib.solution import TObj
+import sys
+
+from v2_MWCCPInstance import v2_MWCCPInstance
+import methods as methods
+
+class v2_MWCCPSolution(PermutationSolution):
+    instance_w: list[list[int]]
+    instance_u: list[int]
+    instance_v: list[int]
+    instance_c: dict[int, list[int]]
+    instance_c_tup: list[(int, int)]
+    instance_adj_v: dict[int, set[int]]
+    instance_edges: list[(int, int, int)]
+
+    to_maximize = False
+    random_order = False
+    x = None
+    w = list[list[int]]
+    prior_obj_val = sys.maxsize
+
+    def __init__(self, inst: v2_MWCCPInstance, init=True, **kwargs):
+        super().__init__(len(inst.get_instance()["u"]), init=False, inst=inst)
+        self.instance_w = inst.get_instance()["w"]
+        self.instance_u = inst.get_instance()["u"]
+        self.instance_v = inst.get_instance()["v"]
+        self.instance_adj_v = inst.get_instance()["adj_v"]
+        self.instance_c = inst.get_instance()["c"]
+        self.instance_c_tup = inst.get_instance()["c_tup"]
+        inst.n = len(self.instance_v)
+
+        self.instance_edges = inst.get_instance()["edges"]
+
+        if init:
+            self.x = np.array(list(self.instance_v))
+
+        self.obj_val_valid = False
+
+    #copy solution instance to copy_solution
+    def copy(self):
+        sol = v2_MWCCPSolution(self.inst)
+        sol.copy_from(self)
+        return sol
+
+    #calculate objective value
+    def calc_objective(self):
+        objective_value = 0
+        position = {v: i for i, v in enumerate(self.x)}
+        for (u1, v1, weight) in self.instance_edges:
+            for (u2, v2, weights) in self.instance_edges:
+                if u1 < u2 and position[v1] > position[v2]:
+                    objective_value += weight + weights
+        return objective_value
+    
+    #deterministic construction heuristic
+    def construct(self, _par, _result):
+        """
+        Construct a solution using a greedy heuristic.
+
+        :return: A feasible permutation of V.
+        """
+        # Step 1: compute average position of each node in V
+        averages = {}
+        V = list(self.instance_v)
+        for v in V:
+            edges_to_v = self.instance_adj_v[v]
+            if edges_to_v:
+                total_position = sum((u) for u in edges_to_v)
+                averages[v] = total_position / len(edges_to_v)
+            else:
+                averages[v] = 0
+
+        # Step 2: sort V by average position
+        sorted_V = sorted(V, key=lambda v: averages[v])
+
+        # Step 3: resolve constraint violations
+        for v, v_prime in self.instance_c.items():
+            for v_prime in self.instance_c[v]:
+                if sorted_V.index(v) >= sorted_V.index(v_prime):
+                    # swap v and v_prime
+                    i = sorted_V.index(v)
+                    j = sorted_V.index(v_prime)
+                    sorted_V[i], sorted_V[j] = sorted_V[j], sorted_V[i]
+
+        self.x = np.array(sorted_V)
+        self.invalidate()
+
+    # random construction heuristic
+    def construct_random(self, _par, _result):
+        '''
+        construction heuristic that iteratively generates random orders of v
+        :param _par:
+        :param _result:
+        :return:
+        '''
+        order = np.arange(self.inst.n)
+        np.random.shuffle(order)
+        x = self.check_order_constraints(order, construct=True)
+        self.x = x
+        current_obj_val = self.calc_objective()
+        if self.is_better_obj(current_obj_val, self.prior_obj_val):
+            self.obj_val = current_obj_val
+        self.prior_obj_val = self.calc_objective()
+
+    # check if new order is valid
+    def check_order_constraints(self, order, construct):
+        '''
+        Used to construct a valid order after a move operator is applied, does not take delta evaluation into
+        account, needs to be extended maybe
+        :param order:
+        :param construct:
+        :return:
+        '''
+        # Repeat until all constraints are satisfied
+        swapped = True
+        #constraint_pairs = {(a, b) for a, values in self.instance_c.items() for b in
+        #                    (values if isinstance(values, list) else [values])}
+        constraint_pairs = self.instance_c_tup
+        arr = np.array([self.x[i] for i in np.nditer(order)])
+        while swapped:
+            swapped = False
+            for a, b in constraint_pairs:
+                indices_a = np.where(arr == a)[0]
+                indices_b = np.where(arr == b)[0]
+                if indices_a.size == 0 or indices_b.size == 0:
+                    continue
+                index_a = indices_a[0]
+                index_b = indices_b[0]
+                if index_a > index_b:
+                    arr[index_a], arr[index_b] = arr[index_b], arr[index_a]
+                    order[index_a], order[index_b] = order[index_b], order[index_a]
+                    swapped = True
+        x = np.array([self.x[i] for i in np.nditer(order)])
+        if construct:
+            return x
+        return order
+    
+    #return true if new order of x is valid
+    def check_order_constraints_bool(self, order):
+        constraint_pairs = self.instance_c_tup
+        arr = np.array([self.x[i] for i in np.nditer(order)])
+        position = {node: idx for idx, node in enumerate(arr)}
+        for v, v_prime in constraint_pairs:
+            if position[v] > position[v_prime]:
+                return False
+        return True
+
+    #local search two exchange neighborhood
+    def ls_two_swap_best(self, _par, _result):
+        self.local_improve_swap("best")
+
+    def ls_two_swap_first(self, _par, _result):
+        self.local_improve_swap("first")
+
+    def ls_two_swap_random(self, _par, _result):
+        self.local_improve_swap("random")
+
+    #local search shift neighborhood
+
+    def ls_shift_best(self, _par, _result):
+        pass
+
+    def ls_shift_first(self, _par, _result):
+        pass
+
+    def ls_shift_random(self, _par, _result):
+        pass
+
+    #local search reverse subinterval neighborhood
+
+    def ls_reverse_best(self, _par, _result):
+        pass
+
+    def ls_reverse_first(self, _par, _result):
+        pass
+
+    def ls_reverse_random(self, _par, _result):
+        pass
+    
+    # local search for two exchange neighborhood
+    def local_improve_swap(self, step):
+        sol = self.copy()
+        n = self.inst.n
+        order = np.arange(n) #to swap every possible position
+        x = sol.x # working on copy
+        current_obj_val = sol.obj_val
+
+        if step == "random":
+            #search through neighborhood randomly
+            #stop search after 10*n iterations
+            is_better_obj = False
+            counter = 0
+
+            while not is_better_obj:
+                p1, p2 = np.random.choice(order, 2, replace=False)
+                order[p1], order[p2] = order[p2], order[p1] #swap positions
+                while self.check_order_constraints_bool(order) == False:
+                    #swap back to original positions
+                    order[p1], order[p2] = order[p2], order[p1]
+                    # get two new positions
+                    p1, p2 = np.random.choice(order, 2, replace=False) #get two random positions
+                    order[p1], order[p2] = order[p2], order[p1]
+
+                #change order of copy x
+                x[p1], x[p2] = x[p2], x[p1]
+                #check if new order is better
+                delta_obj = self.calc_delta_obj_swap(p1, p2, sol)
+                if delta_obj < current_obj_val:
+                    #change actual solution
+                    current_obj_val = delta_obj
+                    self.x[p1], self.x[p2] = self.x[p2], self.x[p1]
+                    self.obj_val = current_obj_val
+                    is_better_obj = True     
+                else:
+                    #change copy back to original order
+                    x[p1], x[p2] = x[p2], x[p1]
+                    #change order back to original order
+                    order[p1], order[p2] = order[p2], order[p1]
+                    counter += 1
+                    #and do random swap again
+                
+                if counter > 10*n:
+                    break
+
+            return is_better_obj
+
+        else:
+            best_sol = sol.obj_val
+            best_p1 = None
+            best_p2 = None
+            if step == "first":
+                best = False
+            else:
+                best = True
+            #search systematically
+
+            for idx, p1 in enumerate(order[:n - 1]):
+                for p2 in order[idx + 1:]:
+                    #change order
+                    order[p1], order[p2] = order[p2], order[p1]
+                    if self.check_order_constraints_bool(order) == False:
+                        #order not valid -> swap back and continue
+                        order[p1], order[p2] = order[p2], order[p1]
+                        continue
+                    else:
+                        #order valid -> check if new order is better
+                        #change order of copy x
+                        x[p1], x[p2] = x[p2], x[p1]
+            
+                        delta_sol = self.calc_delta_obj_swap(p1, p2, sol)
+                        if self.is_better_obj(delta_sol, best_sol):
+
+                            current_obj_val = delta_sol
+                            if best and best_sol > current_obj_val:  # best improvement
+                                best_sol = current_obj_val
+                                best_p1 = p1
+                                best_p2 = p2
+                            else: # first improvement
+                                #self.copy_from(sol)
+                                self.x[p1], self.x[p2] = self.x[p2], self.x[p1]
+                                self.obj_val = current_obj_val
+                                return True
+                    
+                    #change copy x and order back to original order and continue
+                    x[p1], x[p2] = x[p2], x[p1]
+                    order[p1], order[p2] = order[p2], order[p1]
+                                                
+            if best_p1:
+                self.x[best_p1], self.x[best_p2] = self.x[best_p2], self.x[best_p1]
+                self.obj_val = best_sol
+                return True
+            
+
+        return False  
+
+    #calculate delta objective value for swap move
+    def calc_delta_obj_swap(self, p1, p2, sol):
+        delta_old = 0
+        delta_new = 0
+        obj = sol.obj()
+
+
+        if p1 > p2:
+            p1, p2 = p2, p1
+
+        sublist_V = sol.x[p1:p2+1]
+        sublist_U = np.arange(p1 + 1, p2 + 2) #nodes "on top of" the nodes in sublist
+
+        if len(sublist_V) != len(sublist_U):
+            raise ValueError("Sublists are not of the same length")
+        
+        #positions in self.x of the nodes in the sublist
+        position = {v: i for i, v in enumerate(self.x)}
+
+        affected_edges = [(u1, v1, weight) for (u1, v1, weight) in self.instance_edges if v1 in sublist_V or u1 in sublist_U]
+
+        for (u1, v1, w1) in affected_edges:
+            for (u2, v2, w2) in affected_edges:
+                #edges where at most least one node is in the sublist
+                if u1 < u2 and position[v1] > position[v2]:    
+                        delta_old += w1 + w2
+
+        #change positons of the nodes in the sublist (delta evaluation)
+        temp = position[sublist_V[-1]]
+        position[sublist_V[-1]] = position[sublist_V[0]]
+        position[sublist_V[0]] = temp
+
+        for (u1, v1, w1) in affected_edges:
+            for (u2, v2, w2) in affected_edges:
+                #edges where at most least one node is in the sublist
+                if u1 < u2 and position[v1] > position[v2]:    
+                        delta_new += w1 + w2
+
+        new_obj = obj + delta_new - delta_old
+        try_out_self_obj = self.calc_objective()
+        try_out_sol_obj = sol.calc_objective()
+        if new_obj < obj:
+            pass
+        return new_obj if new_obj < obj else obj
+
+    #check if solution is valid
+    def check(self, *args):
+        """
+        Check if valid solution. All constraints must be satisfied.
+
+        :raises ValueError: if problem detected.
+        """
+        x = self.x
+        a = None
+        for a in args:
+            if len(x) > 0:
+                x = a
+        for node, constraint_nodes in self.instance_c.items():
+            for v_prime in self.instance_c[node]:
+                if list(x).index(node) >= list(x).index(v_prime):
+                    if a is not None:
+                        return False
+                    else:
+                        raise ValueError(f"Constraint {node} {v_prime} violated.")
+
+    
+    
+
+
